2b53fe6a10a154a682192ea5b38892d8e579201a
Icaro2M <icaromolina433@gmail.com>
2026-05-16 14:19:10 -0300

    correcao scroll inspetor

--- src/ui/panels/inspector/InspectorPanel.cpp
+++ src/ui/panels/inspector/InspectorPanel.cpp
@@ -46,6 +46,8 @@ void InspectorPanel::draw()
         ImGuiWindowFlags_NoCollapse |
         ImGuiWindowFlags_NoResize |
         ImGuiWindowFlags_NoMove |
+        ImGuiWindowFlags_NoScrollbar |
+        ImGuiWindowFlags_NoScrollWithMouse |
         ImGuiWindowFlags_NoSavedSettings;
 
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(12.0f, 10.0f));
